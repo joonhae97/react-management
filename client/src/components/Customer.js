c9ab47857ae78f0423cb6c73aaeda25916e682c8
joonhae97 <jhae0819@naver.com>
2019-07-02 16:05:11 +0900

    Add CustomerDelete Module

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomerDelete from './CustomerDelete';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 class Customer extends React.Component{
@@ -10,7 +11,8 @@ class Customer extends React.Component{
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
-                <TableCell>{this.props.job}</TableCell> 
+                <TableCell>{this.props.job}</TableCell>
+                <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id = {this.props.id}/></TableCell>
             </TableRow>
 
         )
